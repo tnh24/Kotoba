86deea38fe9c08062493d36356377e5f11df50eb
tnh24 <johnfrp.4k@gmail.com>
2024-05-31 10:57:54 +0630

    G 7

--- grammar.py
+++ grammar.py
@@ -4,8 +4,8 @@ import pandas as pd
 def display_grammar():
     # File and sheet details
     excel_file = '1.xlsx'
-    sheet_names = ['Bonpoe1', 'Bonpoe2', 'Bonpoe3', 'Bonpoe4']
-    subheaders = ['だい １ か 📝', 'だい 2 か 📚', 'だい 3 か 🗒️', 'だい 4 か 🖋️']
+    sheet_names = ['Bonpoe1', 'Bonpoe2', 'Bonpoe3', 'Bonpoe4', 'Bonpoe5', 'Bonpoe6', 'Bonpoe7']
+    subheaders = ['だい １ か 📝', 'だい 2 か 📚', 'だい 3 か 🗒️', 'だい 4 か 🖋️', 'だい 5 か 📖', 'だい 6 か 📜', 'だい 7 か 📚']
 
     # Sidebar for navigation
     selected_subheader = st.sidebar.selectbox("Choose Lesson 📘", subheaders)
