30f081cbbf1a1ea8b97c156c1819b88c118e6161
tnh24 <johnfrp.4k@gmail.com>
2024-06-02 17:55:17 +0630

    Ad K 10

--- meaning.py
+++ meaning.py
@@ -4,8 +4,8 @@ import pandas as pd
 def display_meaning():
     # File and sheet details
     excel_file = '1.xlsx'
-    sheet_names = ['Data1', 'Data2', 'Data3', 'Data4', 'Data5', 'Data6', 'Data7','Data8', 'Data9', 'Verb']
-    subheaders = ['だい １ か 📝', 'だい 2 か 📚', 'だい 3 か 🗒️', 'だい 4 か 🖋️', 'だい 5 か 📖', 'だい 6 か 📜', 'だい 7 か 📚','だい 8 か 📖', 'だい 9 か 📝', 'どうし 🖋️']
+    sheet_names = ['Data1', 'Data2', 'Data3', 'Data4', 'Data5', 'Data6', 'Data7','Data8', 'Data9', 'Data10', 'Verb']
+    subheaders = ['だい １ か 📝', 'だい 2 か 📚', 'だい 3 か 🗒️', 'だい 4 か 🖋️', 'だい 5 か 📖', 'だい 6 か 📜', 'だい 7 か 📚','だい 8 か 📖', 'だい 9 か 📝', 'だい 10 か 📜', 'どうし 🖋️']
 
     # Load data from each sheet
     dfs = {subheader: pd.read_excel(excel_file, sheet_name=sheet, usecols='B:C', header=0) for subheader, sheet in zip(subheaders, sheet_names)}
